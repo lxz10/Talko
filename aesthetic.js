4b3ed95eb47a0e80ceb78b0554ab5d9dfacdd899
lxz10 <30590877+lxz10@users.noreply.github.com>
2019-02-16 22:15:31 +0000

    Add files via upload

--- aesthetic.js
+++ aesthetic.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
   Alert,
   TouchableOpacity
 } from "react-native";
@@ -14,16 +15,28 @@ export default class App extends React.Component {
     render() {
         return (
         <View style = {styles.container}>
-            <Text> Talko </Text>
+            <Text> Let's talko 'bout it. </Text>
         </View>
         );
     }
 }
-    
+
+const TalkoLogo = () => (
+    <Image source = {{uri: "http://i67.tinypic.com/11kilwz.png"}}
+    style = {{width: 200, height: 200}}
+    />
+        container: {
+            alignItems: 'center',
+            justifyContent: 'center',
+        }
+
+)
+export default TalkoLogo
+
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "gold",
+        backgroundColor: "black",
         flex: 1;
         alignItems: 'center',
         justifyContent: 'center',
